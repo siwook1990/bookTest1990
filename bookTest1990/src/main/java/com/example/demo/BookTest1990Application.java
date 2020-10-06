135aa70690159db78ace4bcae6740aa95bcc2327
siwook1030 <siwook1030@naver.com>
2020-10-06 12:17:53 +0900

    슉1990 만듬

---
+++ bookTest1990/src/main/java/com/example/demo/BookTest1990Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTest1990Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookTest1990Application.class, args);
+	}
+
+}
---
+++ bookTest1990/src/test/java/com/example/demo/BookTest1990ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTest1990ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
